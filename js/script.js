182b4b2e80e15e82c1f7bf4b192e668e1995d09e
Smrithi-28 <Smrithi-28>
2026-06-07 17:28:46 +0530

    Updated UI and audio controls

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let speech;
 
 function playAudio() {
 
+    speechSynthesis.cancel();
+
     const text =
         document.getElementById("gallery-content")
         .innerText;
@@ -11,19 +13,12 @@ function playAudio() {
     speech.lang = "en-US";
     speech.rate = 0.95;
     speech.pitch = 1;
+    speech.volume = 1;
 
-    speechSynthesis.cancel();
     speechSynthesis.speak(speech);
 }
 
-function pauseAudio() {
-    speechSynthesis.pause();
-}
-
-function resumeAudio() {
-    speechSynthesis.resume();
-}
-
 function stopAudio() {
+
     speechSynthesis.cancel();
 }
